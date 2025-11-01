3575947cda6d2bf5215bb5fa00e559f94bba4d70
wunluv <santhan@mojahmedia.net>
2025-11-01 21:17:05 +0200

    added new site traffic analytics umami

--- drupal5_theme/page-heavenletters.tpl.php
+++ drupal5_theme/page-heavenletters.tpl.php
@@ -16,17 +16,5 @@
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
   })();
 </script>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-1694227-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
+<script defer src="https://cloud.umami.is/script.js" data-website-id="bcf5183c-c061-4750-821c-963924fe442d"></script>
 </body></html>
